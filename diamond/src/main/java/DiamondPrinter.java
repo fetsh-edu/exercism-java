e9048e7285a1fbb8e922ffba10f555e440f7f75e
fetsh-edu <fetsh.me@gmail.com>
2020-12-27 11:19:44 +0300

    diamond: remove redundand if

--- diamond/src/main/java/DiamondPrinter.java
+++ diamond/src/main/java/DiamondPrinter.java
@@ -9,7 +9,6 @@ class DiamondPrinter {
                 IntStream.rangeClosed('A', a),
                 IntStream.iterate(a - 1, i -> i >= 'A', i -> i - 1)
         ).mapToObj(c -> {
-            if (a == 'A') return "A";
             if (c == 'A') return " ".repeat(a - c).concat("A").concat(" ".repeat(a - c));
             return " ".repeat(a - c).concat(Character.toString(c)).concat(" ".repeat((c - 'A') * 2 - 1)).concat(Character.toString(c)).concat(" ".repeat(a - c));
         }).collect(Collectors.toList());
