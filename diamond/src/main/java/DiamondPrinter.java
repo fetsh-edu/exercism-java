31e3a3694a0c4b35e360398af024003198e8236f
fetsh-edu <fetsh.me@gmail.com>
2020-12-27 11:49:15 +0300

    diamond: Stringbuilder version without ifs

--- diamond/src/main/java/DiamondPrinter.java
+++ diamond/src/main/java/DiamondPrinter.java
@@ -3,14 +3,11 @@ import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
 class DiamondPrinter {
-
     List<String> printToList(char a) {
-        return IntStream.concat(
-                IntStream.rangeClosed('A', a),
-                IntStream.iterate(a - 1, i -> i >= 'A', i -> i - 1)
-        ).mapToObj(c -> {
-            if (c == 'A') return " ".repeat(a - c).concat("A").concat(" ".repeat(a - c));
-            return " ".repeat(a - c).concat(Character.toString(c)).concat(" ".repeat((c - 'A') * 2 - 1)).concat(Character.toString(c)).concat(" ".repeat(a - c));
-        }).collect(Collectors.toList());
+        return IntStream.concat(IntStream.rangeClosed('A', a), IntStream.iterate(a - 1, i -> i >= 'A', i -> i - 1))
+                .mapToObj(c -> { StringBuilder line = new StringBuilder(" ".repeat((a - 'A') * 2 + 1));
+                    line.setCharAt(a - c, (char) c); line.setCharAt(line.length() - 1 - (a - c), (char) c);
+                    return line.toString();
+                }).collect(Collectors.toList());
     }
 }
